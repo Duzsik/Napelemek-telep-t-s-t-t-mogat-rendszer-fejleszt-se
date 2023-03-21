391e2e34219822460e0797a69ff7e8f501c2708c
Duzsik <pelsodominik@gmail.com>
2023-03-21 22:23:39 +0100

    Database connection
    Database connecton works, and test table created

--- Napelem/obj/Debug/net6.0-windows/admin.g.i.cs
+++ Napelem/obj/Debug/net6.0-windows/admin.g.i.cs
@@ -93,7 +93,7 @@ namespace Napelem {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Napelem;component/admin.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Napelem;V1.0.0.0;component/admin.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\admin.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- Napelem/obj/Debug/net6.0-windows/raktarVezeto.g.i.cs
+++ Napelem/obj/Debug/net6.0-windows/raktarVezeto.g.i.cs
@@ -61,7 +61,7 @@ namespace Napelem {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Napelem;component/raktarvezeto.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Napelem;V1.0.0.0;component/raktarvezeto.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\raktarVezeto.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
