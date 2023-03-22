25a671a2312ca367d61301ab078baf783c3d0586
KleinGergo <klein.gergo2000@gmail.com>
2023-03-22 21:01:20 +0100

    Created database tables

--- Napelem/Database/Database.cs
+++ Napelem/Database/Database.cs
@@ -3,11 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
-namespace Napelem.Migrations
+namespace Napelem.Database
 {
+    public class Database
+    {
+        public void CreateDatabaseConnection()
+        {
+            // Connection string parameters
+            string host = "localhost";
+            string database = "mydatabase";
+            string username = "myusername";
+            string password = "mypassword";
+
+            // Build connection string
+            string connString = $"Host={host};Database={database};Username={username};Password={password}";
+
+            // Create connection object
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                try
+                {
+                    // Open connection
+                    conn.Open();
+
+                    // Do something with the connection
+                    Console.WriteLine("Connection opened successfully.");
+
+                    // Close connection
+                    conn.Close();
+                    Console.WriteLine("Connection closed successfully.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+
+    }
+    
 
 
-  
+
 }
