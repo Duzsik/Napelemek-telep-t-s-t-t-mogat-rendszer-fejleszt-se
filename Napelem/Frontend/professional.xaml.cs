75cecf688d671d1ac6e0d6264fd1bc6447b5b03a
Duzsik <pelsodominik@gmail.com>
2023-05-10 15:44:05 +0200

    LoadComponents and LoadProject added to professional

--- Napelem/Frontend/professional.xaml.cs
+++ Napelem/Frontend/professional.xaml.cs
@@ -11,12 +11,22 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Napelem.Models;
+using System.Net.Http;
 
 namespace Napelem
 {
     /// <summary>
     /// Interaction logic for professional.xaml
     /// </summary>
+
+    public class ProjectComponent
+    {
+        public Project Project { get; set; }
+        public Component Component { get; set; }
+    }
     public partial class professional : Window
     {
         void exit()
@@ -29,11 +39,41 @@ namespace Napelem
         {
             InitializeComponent();
         }
-
-
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        public async void LoadCompontents()
         {
+            using var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7186/");
 
+            var response = await client.GetAsync($"api/Component/SendComponent");
+            if (response.IsSuccessStatusCode == true)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                var obj = JsonConvert.DeserializeObject<JObject>(json);
+                var componentsJson = obj["value"].ToString();
+                var components = JsonConvert.DeserializeObject<List<Component>>(componentsJson);
+                for (int i = 0; i < components.Count; i++)
+                {                   
+                    assetSelectComboBox.Items.Add(components[i].componentID + " " + components[i].name);
+                }
+            }
+        }
+        public async void LoadProjects()
+        {
+            using var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7186/");
+
+            var response = await client.GetAsync($"api/Project/ListProjects");
+            if (response.IsSuccessStatusCode == true)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                var obj = JsonConvert.DeserializeObject<JObject>(json);
+                var projectsJson = obj["value"].ToString();
+                var projects = JsonConvert.DeserializeObject<List<Project>>(projectsJson);
+                for (int i = 0; i < projects.Count; i++)
+                {
+                    ProjectIdComboBox.Items.Add(projects[i].projectID + " " + projects[i].name);
+                }
+            }
         }
 
         private void newProject_exit1(object sender, RoutedEventArgs e)
